f47291f0113acf61748d4792e2ccce59e416f236
Ravini28 <ravinimaneesha@gmail.com>
2023-12-09 08:18:33 +0530

    Initial Commit

---
+++ src/main/java/com/issuemanagementservice/issuemanagementservice/IssueManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.issuemanagementservice.issuemanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/issuemanagementservice/issuemanagementservice/IssueManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.issuemanagementservice.issuemanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
